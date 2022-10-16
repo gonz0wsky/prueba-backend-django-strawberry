6e9ebd77fe67203c6f25064f5c6e882d18564ebe
Álvaro González <45352512+gonz0wsky@users.noreply.github.com>
2022-10-17 00:07:25 +0200

    feat: add user db model

--- historiasapp/users/models.py
+++ historiasapp/users/models.py
@@ -1,3 +1,14 @@
+from enum import unique
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    email = models.CharField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=30)
+    hash = models.CharField(max_length=128)
+    id = models.UUIDField(unique=True, primary_key=True)
+    last_name = models.CharField(max_length=30)
+    modified_at = models.DateTimeField(auto_now=True)
+    username = models.CharField(max_length=40)
